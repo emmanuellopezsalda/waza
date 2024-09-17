23f30e31fd74faa2d894f78440677408beb0155e
emmanuellopezsalda <emmanuellopezsalda123@gmail.com>
2024-09-17 15:53:49 -0500

    blow job

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,22 +1,13 @@
-let chat = document.querySelector(".chat");
-
-const HEADER = document.querySelector(".header-chat");
-const CONTENT_CHAT = document.querySelector(".content-chat");
-const MESSAGE_INPUT = document.querySelector(".message-input");
-
-let background = true;
-
-chat.addEventListener("click", () => {
-    if (background) {
-        chat.style.backgroundColor = "#202c33";
-        HEADER.style.display = "flex";
-        CONTENT_CHAT.style.display = "flex";
-        MESSAGE_INPUT.style.opacity = "1";
-    } else {
-        chat.style.backgroundColor = "#111b21";
-        HEADER.style.display = "none";
-        CONTENT_CHAT.style.display = "none";
-        MESSAGE_INPUT.style.opacity = "0";        
-    }
-    background = !background;
-})
+let chat = document.querySelectorAll(".chat-item");
+let openchat = false;
+const CONTENT_CHAT = document.querySelector(".chat-window");            
+const WELCOME_MESSAGE = document.querySelector(".welcome-message");
+chat.forEach(btn => {
+    btn.addEventListener("click", () => {
+        openchat = true;
+        if (openchat) {
+            CONTENT_CHAT.style.display = "flex";
+            WELCOME_MESSAGE.style.display = "none";
+        }
+    })
+});
